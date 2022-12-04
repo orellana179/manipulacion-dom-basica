b00cef0a0febe13038692219e20afd5a807785f6
Kevin Orellana <alexanderorellana867@gmail.com>
2022-12-04 19:32:23 +0100

    My first commit in the proyect

---
+++ index.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafo = document.querySelector('.parrafo');
+const parrafoId  = document.querySelector('#parrafoId');
+const input = document.querySelector('input');
+
+console.log({
+    title,
+    p,
+    parrafo,
+    parrafoId,
+    input
+});
