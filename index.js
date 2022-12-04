d3e738bab83a8b8ace13805efe565dab1c6a6224
Kevin Orellana <alexanderorellana867@gmail.com>
2022-12-04 20:03:02 +0100

    Manipulamos elementos y atributos html con javascript

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ console.log({
     parrafoId,
     input
 });
+
+title.innerHTML = "Hello world";
+title.innerText = 'Hola Mundo';
+
+//Editar atributos
+title.getAttribute('class');
+title.setAttribute('class', 'titulo');
+
+//Agregar atributos
+title.classList.add('oscuro');
+title.classList.remove('oscuro');
+
+// title.classList.toggle('oscuro');
+// title.classList.contains('oscuro');
+
+input.value = '456';
+const img = document.createElement('img');
+img.setAttribute('src','https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4wHeT?ver=80f6');
+console.log(img);
+
+parrafoId.innerHTML = '';
+parrafoId.appendChild(img);
